3dba19ec976862362687d2cf119bda6d9bac9470
fyy <1040122743@qq.com>
2022-11-27 19:43:31 +0800

    2-9 增加自定义配置项示例

--- src/main/java/com/my/wiki/controller/TestController.java
+++ src/main/java/com/my/wiki/controller/TestController.java
@@ -1,12 +1,16 @@
 package com.my.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    @Value("${test.hello}")
+    private String testHello;
+
     @RequestMapping("/hello")
     public String hello(){
-        return "Hello world!";
+        return "Hello world!"+testHello;
     }
 }
